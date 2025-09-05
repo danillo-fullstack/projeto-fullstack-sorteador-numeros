a558f48b2fe63dc4e74fb7533ba2d25c47212eb9
Danillo Victtor <danilo_victor2004@hotmail.com>
2025-09-05 08:43:16 -0300

    Finalizado o evento click dos botões.

--- scripts.js
+++ scripts.js
@@ -1,6 +1,20 @@
 const track = document.querySelector('#switch');
+const sortear = document.querySelector('.show-form form .draw');
+const showForm = document.querySelector('.show-form');
+const showResult = document.querySelector('.show-result');
+const sortearNovamente = document.querySelector('.show-result .draw');
 
 track.addEventListener('click', () => {
     const button = document.querySelector('#switch button');
     button.classList.toggle('switch-uncheck');
+});
+
+sortear.addEventListener('click', () => {
+    showForm.classList.add('hide');
+    showResult.classList.remove('hide')
+});
+
+sortearNovamente.addEventListener('click', () => {
+    showForm.classList.remove('hide');
+    showResult.classList.add('hide');
 })
