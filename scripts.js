75013d473b50a4194006980390ace7ea861f5b5f
Danillo Victtor <danilo_victor2004@hotmail.com>
2025-09-17 10:25:38 -0300

    Exige que os inputs sejam preenchidos.

--- scripts.js
+++ scripts.js
@@ -1,15 +1,22 @@
+const form = document.querySelector('.show-form form')
 const track = document.querySelector('#switch');
 const sortear = document.querySelector('.show-form form .draw');
 const showForm = document.querySelector('.show-form');
 const showResult = document.querySelector('.show-result');
 const sortearNovamente = document.querySelector('.show-result .draw');
 
+// seleciona os inputs do form
+const number = document.querySelector('#number');
+const minNumber = document.querySelector('#min-number');
+const maxNumber = document.querySelector('#max-number');
+
 track.addEventListener('click', () => {
     const button = document.querySelector('#switch button');
     button.classList.toggle('switch-uncheck');
 });
 
-sortear.addEventListener('click', () => {
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
     showForm.classList.add('hide');
     showResult.classList.remove('hide')
 });
@@ -17,4 +24,12 @@ sortear.addEventListener('click', () => {
 sortearNovamente.addEventListener('click', () => {
     showForm.classList.remove('hide');
     showResult.classList.add('hide');
-})
+    inputClear();
+});
+
+function inputClear() {
+    number.value = "";
+    minNumber.value = "";
+    maxNumber.value = "";
+    number.focus();
+}
